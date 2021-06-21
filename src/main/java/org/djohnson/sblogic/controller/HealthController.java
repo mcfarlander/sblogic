39f31a07190abd827c1672a9890a46e0fd5d5014
David Johnson <mcfarlander@charter.net>
2021-06-21 07:35:54 -0500

    autowired the health service to the controller

--- src/main/java/org/djohnson/sblogic/controller/HealthController.java
+++ src/main/java/org/djohnson/sblogic/controller/HealthController.java
@@ -1,7 +1,10 @@
 package org.djohnson.sblogic.controller;
 
+import org.djohnson.sblogic.model.HealthResult;
+import org.djohnson.sblogic.service.HealthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,22 @@ public class HealthController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HealthController.class);
 	
+	@Autowired
+	HealthService healthService;
+	
+	/**
+	 * Get the health status of the application.
+	 * 
+	 * @return {@link HealthResult} the health status.
+	 */
+	@RequestMapping("/health")
+	public HealthResult getHealthStatus() {
+		
+		logger.debug("getting the health service result");
+		
+		return healthService.getHealthResult();
+		
+	}
 	
 
 }
