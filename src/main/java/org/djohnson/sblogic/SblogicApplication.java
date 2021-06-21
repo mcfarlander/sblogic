a92ab8fa002f40a94a7f836cb254f47ac17f3544
David Johnson <mcfarlander@charter.net>
2021-06-21 13:53:08 -0500

    added application annotations for repo pattern, entity

--- src/main/java/org/djohnson/sblogic/SblogicApplication.java
+++ src/main/java/org/djohnson/sblogic/SblogicApplication.java
@@ -2,10 +2,16 @@ package org.djohnson.sblogic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"org.djohnson.sblogic"})
+@EntityScan(basePackages="{org.djohnson.sblogic.model")
+@EnableJpaRepositories(basePackages="{org.djohnson.sblogic.respository")
+@EnableTransactionManagement
 public class SblogicApplication {
 
 	public static void main(String[] args) {
